d4b77d7d650e393cf9ab2c4a9a1218e8a51d219a
tantinuraini <tantinuraini@apps.ipb.ac.id>
2026-09-17 20:34:55 +0000

    Menambahkan fitur input data panen

--- input_panen.py
+++ input_panen.py
@@ -1,18 +1,27 @@
+
 # ===================================================
-# FITUR 1: Input Data Panen (Dikerjakan Anggota A)
+# FITUR 1: Input Data Panen
+# Dikerjakan Anggota A
 # Branch: feature-input
 # ===================================================
 
 def input_data_panen():
     """
-    Fungsi untuk menerima input data transaksi panen dari pengguna.
-    Mengembalikan data berupa Dictionary.
+    Fungsi untuk menerima input data transaksi panen.
+    Mengembalikan data dalam bentuk dictionary.
     """
     print("=== INPUT DATA HASIL PANEN ===")
-    nama_komoditas = input("Masukkan nama komoditas (misal: Padi/Jagung): ")
-    harga_per_kg = float(input("Masukkan harga per kg (Rp): "))
-    jumlah_kg = float(input("Masukkan jumlah panen (kg): "))
-    
+
+    nama_komoditas = input(
+        "Masukkan nama komoditas (misal: Padi/Jagung): "
+    )
+    harga_per_kg = float(
+        input("Masukkan harga per kg (Rp): ")
+    )
+    jumlah_kg = float(
+        input("Masukkan jumlah panen (kg): ")
+    )
+
     return {
         "komoditas": nama_komoditas,
         "harga_per_kg": harga_per_kg,
