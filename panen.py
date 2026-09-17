979877d86b26f308294ed134bb8d064f9f8fceea
tantinuraini <tantinuraini@apps.ipb.ac.id>
2026-09-17 20:22:17 +0000

    Menghapus file panen.py

--- panen.py
+++
@@ -1,9 +0,0 @@
-def hitung_total(jumlah_kg, harga_per_kg):
-    return jumlah_kg * harga_per_kg
-
-def hitung_diskon(total, persen_diskon):
-    return total - (total * (persen_diskon / 100))
-
-total_awal = hitung_total(100, 5000)
-print("Total awal:", total_awal)
-print("Total setelah diskon 10%:", hitung_diskon(total_awal, 10))
